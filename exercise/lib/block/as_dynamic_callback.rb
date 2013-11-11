4a33e545a0feecae3df80c428b87b676eac45adf
Nicholas Ren <ren.xiaojun@rea-group.com>
2013-11-11 17:52:09 +0800

    Add homework

--- exercise/lib/block/as_dynamic_callback.rb
+++ exercise/lib/block/as_dynamic_callback.rb
@@ -1,24 +1,13 @@
 class Server
-  def self.generate(&block)
-    server = Server.new
-    server.instance_eval(&block)
-    server
+  def self.draw(&block)
+    Server.new
   end
 
   def initialize()
     @handlers = {}
   end
 
-  def handle(pattern, &block)
-    @handlers[pattern] = block
-  end
-
-  def recieve(message)
-    @handlers.each do |pattern,block|
-      if match = message.match(pattern)
-        return block.call(match)
-      end
-    end
-    return nil
+  def recieve(method, uri)
+    "#{method} #{uri}"
   end
 end
--- exercise/spec/block/as_dynamic_callback_spec.rb
+++ exercise/spec/block/as_dynamic_callback_spec.rb
@@ -2,26 +2,64 @@ require 'spec_helper'
 
 describe Server do
   let(:server) {
-    Server.generate do
-      handle(/hello/i) { "Hello from server" }
-      handle(/play/i) { "play from server" }
+    Server.draw do
+      get '/user' do
+        "This is the user response"
+      end
+
+      post '/user' do
+        "User created"
+      end
+
+      put '/user/:id' do
+        "User #{params[:id]} updated"
+      end
+
+      not_found do
+        'not found'
+      end
     end
   }
 
-  subject { server.recieve(request) }
+  subject { server.recieve(method, uri) }
 
-  context "matched" do
-    let(:request) { "hello" }
-    it "should handle matched pattern" do
-      subject.should eq("Hello from server")
+  context "get" do
+    let(:method) { 'GET' }
+    let(:uri) { "/user" }
+    it "be responded" do
+      subject.should eq("This is the user response")
     end
   end
 
-  context "not matched" do
-    let(:request) { "not matched" }
-    it "shooud reutrn nil" do
-      subject.should be_nil
+  context "post" do
+    let(:method) { 'POST' }
+    let(:uri) { "/user" }
+    it "be responded" do
+      subject.should eq("User created")
     end
   end
 
+  context "put" do
+    let(:method) { 'PUT' }
+    let(:uri) { "/user/1" }
+    it "be responded" do
+      subject.should eq("User 1 updated created")
+    end
+  end
+
+  context "method not matched" do
+    let(:method) { 'DELETE' }
+    let(:uri) { "/user" }
+    it "be responded" do
+      subject.should eq('not found')
+    end
+  end
+
+  context "uri not matched" do
+    let(:method) { 'GET' }
+    let(:uri) { "/dogs" }
+    it "be responded" do
+      subject.should eq('not found')
+    end
+  end
 end
