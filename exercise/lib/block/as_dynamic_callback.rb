42f6035148269ef16199310a3b38caaadf9b3354
Nicholas Ren <ren.xiaojun@rea-group.com>
2013-11-25 22:29:22 +0800

    move param into handler.

--- exercise/lib/block/as_dynamic_callback.rb
+++ exercise/lib/block/as_dynamic_callback.rb
@@ -1,11 +1,53 @@
 class Server
   PARAM_PATTERN = /:([a-z]+)/
 
-  class Handler
-    attr_reader :pattern, :block
+  def self.draw(&block)
+    server = Server.new
+    server.instance_eval(&block)
+    server
+  end
 
-    def initialize(server, pattern, block)
-      @server = server
+  def initialize
+    @handlers = {}
+  end
+
+  %w{ get post put }.each do |method|
+    define_method(method) do |uri, &block|
+      handlers_for(method) << Handler.new(build_pattern(uri), block)
+    end
+  end
+
+  def not_found(&block)
+    @not_found = block
+  end
+
+  def recieve(method, uri)
+    handlers_for(method).each do |handler|
+      response = handler.handle(uri)
+      return response unless response.nil?
+    end
+    @not_found.call
+  end
+
+  private
+  def handlers_for(method)
+    @handlers[method.downcase] ||= []
+  end
+
+  def build_pattern(uri)
+    match_data = uri.match PARAM_PATTERN
+    if match_data
+      param_name = match_data.to_a.last
+      Regexp.new(uri.gsub(PARAM_PATTERN, "(?<#{param_name}>[^/]+)"))
+    else
+      Regexp.new(uri)
+    end
+  end
+
+  class Handler
+    attr_reader :pattern, :block, :params
+
+    def initialize(pattern, block)
       @pattern = pattern
       @block = block
     end
@@ -13,9 +55,8 @@ class Server
     def handle uri
       match_data = @pattern.match uri
       if match_data
-        param = extract_params(match_data)
-        @server.add_param(param) if param
-        @block.call
+        @params = extract_params(match_data)
+        self.instance_eval(&@block)
       end
     end
 
@@ -27,58 +68,5 @@ class Server
       end
     end
 
-  end
-
-  def self.draw(&block)
-    server = Server.new
-    server.instance_eval(&block)
-    server
-  end
-
-  %w{ get post put }.each do |method|
-    define_method(method) do |uri, &block|
-      pattern = build_pattern(uri)
-
-      @handlers[method] ||= []
-      @handlers[method] << Handler.new(self, pattern, block)
-    end
-  end
-
-  def not_found(&block)
-    @not_found = block
-  end
-
-  def initialize()
-    @handlers = {}
-  end
-
-  def add_param param
-    self.params.merge! param
-  end
-
-  def recieve(method, uri)
-    handlers_for(method).each do |handler|
-      response = handler.handle(uri)
-      return response unless response.nil?
-    end
-    @not_found.call
-  end
-
-  def handlers_for(method)
-    @handlers[method.downcase] || []
-  end
-
-  def params
-    @params ||= Hash.new
-  end
-
-  def build_pattern(uri)
-    match_data = uri.match PARAM_PATTERN
-    if match_data
-      param_name = match_data.to_a.last
-      Regexp.new(uri.gsub(PARAM_PATTERN, "(?<#{param_name}>[^/]+)"))
-    else
-      Regexp.new(uri)
-    end
   end
 end
