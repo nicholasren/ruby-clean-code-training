19265f8d6908b251b0fd8b9f33104dee47bd4596
Nicholas Ren <ren.xiaojun@rea-group.com>
2013-12-06 19:30:17 +0800

    Add internal dsl.

---
+++ examples/spec/internal_dsl/house_builder_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe HouseBuilder do
+  subject {
+    HouseBuilder.build :home do
+      floor(1) {
+        room :den
+        room :kitchen
+      }
+
+      floor(2) {
+        room :bedroom
+        room :bathroom
+      }
+    end
+  }
+
+  its(:name) { should eq(:home) }
+  context "house builder" do
+    it "should have 2 floors" do
+      subject.floors.size.should eq(2)
+    end
+  end
+end
